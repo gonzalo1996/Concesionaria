6bda0047eff20ae92fdba2b6bc9a3bff3ab506cb
gonza <gonzalo-96-09@hotmail.com>
2019-06-10 12:14:56 -0300

    ejecutable
    borrado de importaciones innecesarias para que a juanga no le tire error

--- Ejecutable.java
+++ Ejecutable.java
@@ -1,7 +1,3 @@
-import Model.*;
-import Utils.Fecha;
-import jdk.internal.util.xml.impl.Input;
-
 public class Ejecutable {
 
     public static void main(String[] args) {
@@ -11,15 +7,7 @@ public class Ejecutable {
         //Domicilio domicilio = new Domicilio("25 de mayo","5551","9","h","7600","Mar del Plata");
                 //String nombre, String apellido, String dni, String fecha_nacimiento, String email, int edad, String telefono, Domicilio domicilio
         //Persona p = new Persona( "gonzalo","rodriguez","39732211","16/07/96","gonzalo-96-09@hotmail.com",22,"2234368995",domicilio);
-
-
-
-
-
-
-
-
-
+        
     }
 
 }
