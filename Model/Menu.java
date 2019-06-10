c5dbc942db648a425e89a5680b96484ae4a36d81
juansanmartich <juannba1@hotmail.com>
2019-06-10 12:21:28 -0300

    Comit Class Menu
    Agregue los sout del menu visual.

--- Model/Menu.java
+++ Model/Menu.java
@@ -26,9 +26,9 @@ public class Menu {
         ControladoraUsuario.mostrar(
                 "1_ Administrar Vendedores" +
                 "\n2_ Menu Vehiculos." +
-                "\n3_ Menu Ventas." +
+                "\n3_ Menu Vendedor." +
                 "\n4_ Administrar agencia."+
-                "\n5_ salir");
+                "\n5_ Salir");
     }
 
     public void menu_admin_opcion_1(){
@@ -41,5 +41,44 @@ public class Menu {
                 "\n6_ Ventas de vendedores."+
                 "\n7_ Volver menu.");
     }
+
+    public void menu_vehiculo () {
+        ControladoraUsuario.mostrar(
+                "1_ Listar Vehculos." +
+                        "\n 2_ Mostrar Vehiculo." +
+                        "\n 3_ Agregar Vehiculo" +
+                        "\n 4_ Dar baja vehiculo." +
+                        "\n 5_ Modificar Vehiculo." +
+                        "\n 6_ Volver al menu."
+
+        );
+    }
+
+    public void menu_vendedor () {
+        ControladoraUsuario.mostrar(
+                "1_ Vender Vehiculo." +
+                        "\n2_ Listar cantidad de ventas totales." +
+                        "\n3_ Ver ventas por mes especifico."+
+                        "\n4_ Stock disponible."+
+                        "\n4_ Volver al menu."
+        );
+
+    }
+
+    public void menu_Administrar_Agencia (){
+            ControladoraUsuario.mostrar(
+                    "1_ Agregar gastos."+
+                            "\nGanancia total."+
+                            "\nListar gastos mensuales"+
+                            "\nAbrir sitema de sugestion."
+            );
+    }
+
+    public void menu_Sugerencia (){
+        ControladoraUsuario.mostrar(
+                ""
+        );
+    }
+
 
 }
