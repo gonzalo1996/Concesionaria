8eb359e192275c4bfc67980e74d2d2e8ca70b900
juansanmartich <juannba1@hotmail.com>
2019-06-11 12:53:30 -0300

    Menu
    Nuevo menu agregado.

--- Model/Menu.java
+++ Model/Menu.java
@@ -10,6 +10,7 @@ import javax.naming.ldap.Control;
 public class Menu {
 
     public void menu(){
+
     }
 
     public void inicio(){
@@ -17,28 +18,67 @@ public class Menu {
                 "Bienvenido." +
                 "\nIngrese usuario: ");
 
-                ControladoraUsuario.ingresarString();
+        ControladoraUsuario.ingresarString();
         ControladoraUsuario.mostrar("\nIngrese contraseña: ");
     }
 
     public void menu_admin(){
         ControladoraUsuario.mostrar(
                 "1_ Administrar Vendedores" +
-                "\n2_ Menu Vehiculos." +
-                "\n3_ Menu Ventas." +
-                "\n4_ Administrar agencia."+
-                "\n5_ salir");
+                        "\n2_ Menu Vehiculos." +
+                        "\n3_ Menu Vendedor." +
+                        "\n4_ Administrar agencia."+
+                        "\n5_ Salir");
     }
 
     public void menu_admin_opcion_1(){
         ControladoraUsuario.mostrar(
                 "1_ Agregar vendedor." +
-                "\n2_ Mostrar vendedores." +
-                "\n3_ Mostrar vendedor especifico." +
-                "\n4_ Modificar info vendedor." +
-                "\n5_ Buscar vendedor" +
-                "\n6_ Ventas de vendedores."+
-                "\n7_ Volver menu.");
+                        "\n2_ Mostrar vendedores." +
+                        "\n3_ Mostrar vendedor especifico." +
+                        "\n4_ Modificar info vendedor." +
+                        "\n5_ Buscar vendedor" +
+                        "\n6_ Ventas de vendedores."+
+                        "\n7_ Volver menu.");
     }
+
+    public void menu_vehiculo () {
+        ControladoraUsuario.mostrar(
+                "1_ Listar Vehculos." +
+                        "\n 2_ Mostrar Vehiculo." +
+                        "\n 3_ Agregar Vehiculo" +
+                        "\n 4_ Dar baja vehiculo." +
+                        "\n 5_ Modificar Vehiculo." +
+                        "\n 6_ Volver al menu."
+
+        );
+    }
+
+    public void menu_vendedor () {
+        ControladoraUsuario.mostrar(
+                "1_ Vender Vehiculo." +
+                        "\n2_ Listar cantidad de ventas totales." +
+                        "\n3_ Ver ventas por mes especifico."+
+                        "\n4_ Stock disponible."+
+                        "\n4_ Volver al menu."
+        );
+
+    }
+
+    public void menu_Administrar_Agencia (){
+        ControladoraUsuario.mostrar(
+                "1_ Agregar gastos."+
+                        "\n1_ Ganancia total."+
+                        "\n2_ Listar gastos mensuales"+
+                        "\n3_ Abrir sitema de sugestion."
+        );
+    }
+
+    public void menu_Sugerencia (){
+        ControladoraUsuario.mostrar(
+                ""
+        );
+    }
+
 
 }
